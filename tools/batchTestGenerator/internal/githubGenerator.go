e173c41d841ee40eb7d2f3cac1f324468d4ef8d3
Pavan Sai Vasireddy (Vasi) <pvasir@amazon.com>
2023-01-04 05:30:17 -0800

    test

--- tools/batchTestGenerator/internal/githubGenerator.go
+++ tools/batchTestGenerator/internal/githubGenerator.go
@@ -52,12 +52,12 @@ func GithubGenerator(config RunConfig) error {
 	}
 	defer ghEnv.Close()
 
-	_, err = ghEnv.WriteString(fmt.Sprintf(`batch-keys=%s\n`, githubBatchKeys))
+	_, err = ghEnv.WriteString(fmt.Sprintf(`batch-keys=%s \n`, githubBatchKeys))
 	if err != nil {
 		fmt.Printf("error writing githubBatchKeys in GITHUB_OUTPUT: %v", err)
 	}
 
-	_, err = ghEnv.WriteString(fmt.Sprintf(`batch-values=%s\n`, githubBatchValues))
+	_, err = ghEnv.WriteString(fmt.Sprintf(`batch-values=%s \n`, githubBatchValues))
 	if err != nil {
 		fmt.Printf("error writing githubBatchValues in GITHUB_OUTPUT: %v", err)
 	}
